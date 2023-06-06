95e60eb161891b6fd5cecd25fe74ae8a8f537850
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2023-06-06 00:22:22 -0300

    ex015 concluido

---
+++ exercicios/aula12ex/ex015/script.js
@@ -0,0 +1,28 @@
+function verify() {
+    var ano = document.querySelector('input#iano').value
+    var sexo = document.querySelector('input[name="sexo"]:checked').value
+    var img = document.getElementById('foto')
+    console.log(ano)
+    console.log(sexo)
+    
+    if((ano < 13) && (sexo == "M")) {
+        img.src = 'img/crincaM.jpg'
+    }else if((ano < 13) && (sexo == "F")) {
+        img.src = 'img/crincaF.jpg'
+    }else if((ano < 18) && (sexo == "M")) {
+        img.src = 'img/adolescenteM.jpg'
+    }else if((ano < 18) && (sexo == "F")) {
+        img.src = 'img/adolescenteF.jpg'
+    }else if((ano < 50) && (sexo == "M")) {
+        img.src = 'img/adultoM.jpg'
+    }else if((ano < 50) && (sexo == "F")) {
+        img.src = 'img/adultoF.jpg'
+    }else if((ano <= 100) && (sexo == "M")) {
+        img.src = 'img/velho.jpg'
+    }else if((ano <= 100) && (sexo == "F")) {
+        img.src = 'img/velha.jpg'
+    }else if((ano <= 0) || (ano > 100)) {
+        alert("Idade invalida, tente novamente")
+    }
+    
+}
