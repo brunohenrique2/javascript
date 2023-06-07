7ce36f08abf3e77d2b2d59140096cc8b20cba558
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2023-06-07 13:29:15 -0300

    ex015 corrigido

--- exercicios/aula12ex/ex015/script.js
+++ exercicios/aula12ex/ex015/script.js
@@ -5,21 +5,21 @@ function verify() {
     console.log(ano)
     console.log(sexo)
     
-    if((ano < 13) && (sexo == "M")) {
+    if((ano >= 1 && ano < 13) && (sexo == "M")) {
         img.src = 'img/crincaM.jpg'
-    }else if((ano < 13) && (sexo == "F")) {
+    }else if((ano >= 1 && ano < 13) && (sexo == "F")) {
         img.src = 'img/crincaF.jpg'
-    }else if((ano < 18) && (sexo == "M")) {
+    }else if((ano >= 13 && ano < 18) && (sexo == "M")) {
         img.src = 'img/adolescenteM.jpg'
-    }else if((ano < 18) && (sexo == "F")) {
+    }else if((ano >= 13 && ano < 18) && (sexo == "F")) {
         img.src = 'img/adolescenteF.jpg'
-    }else if((ano < 50) && (sexo == "M")) {
+    }else if((ano >= 18 && ano < 50) && (sexo == "M")) {
         img.src = 'img/adultoM.jpg'
-    }else if((ano < 50) && (sexo == "F")) {
+    }else if((ano >= 18 && ano < 50) && (sexo == "F")) {
         img.src = 'img/adultoF.jpg'
-    }else if((ano <= 100) && (sexo == "M")) {
+    }else if((ano >= 50 && ano <= 100) && (sexo == "M")) {
         img.src = 'img/velho.jpg'
-    }else if((ano <= 100) && (sexo == "F")) {
+    }else if((ano >= 50 && ano <= 100) && (sexo == "F")) {
         img.src = 'img/velha.jpg'
     }else if((ano <= 0) || (ano > 100)) {
         alert("Idade invalida, tente novamente")
