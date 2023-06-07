5786067f072e18c5f077e7d448c3238ee13e4a1a
Bruno Soares <72941170+brunohenrique2@users.noreply.github.com>
2023-06-07 18:18:05 -0300

    ex016 concluido e com plus

---
+++ exercicios/aula12ex/ex016/script.js
@@ -0,0 +1,28 @@
+function carregar() {
+    var msg = document.getElementById("msg")
+    var img = document.getElementById("imagem")
+    var body = document.getElementById('body')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    var seg = data.getSeconds()
+    msg.innerText = `Agora são ${hora}:${min}:${seg} horas`
+
+    if(hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'img/manha.jpg'
+        body.style.backgroundColor = '#FCE78A'
+    }else if(hora >= 12 && hora < 18) {
+        //BOA TARDE
+        img.src = 'img/tarde.jpg'
+        body.style.backgroundColor = '#B5966C'
+    }else if(hora >= 18 && hora < 24) {
+        //BOA NOITE
+        img.src = 'img/noite.jpg'
+        body.style.backgroundColor = '#0D273E'
+
+    }
+
+    setInterval(carregar, 1000)
+
+}
